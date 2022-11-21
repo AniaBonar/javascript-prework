91721ec6a3c4436ce8b8523489953eca25751d0c
Anna Bonar <saska.anna@wp.pl>
2022-11-21 23:30:12 +0100

    Add computer moves

--- js/script.js
+++ js/script.js
@@ -1,6 +1,29 @@
 let computerMove = `kamień`;
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!')
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-let playerMove = 'papier';
-printMessage ('Zagrałem ' + playerMove + '! Jeśli Twój ruch to noźyce, to wygrywasz!')
+
+let computerMove = `kamień`;
+let playerMove = 'nieznany ruch';
+
+printMassage('Zagrałem ' + computerMove  + '! Jeśli Twój ruch to nożyczki, to wygrywasz!'); 
+
+if(playerMove == 1){
+  computerMove = 'papier';
+}
+printMassage ( 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to przegrywasz!');
+
+else if(playerMove == 2){
+	computerMove = 'nożyce';
+}
+
+printMassage( 'Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to przegrywasz!');
+
+else if(playerMove == 3){
+	computerMove = 'kamień';
+
+}
+printMassage ('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamień, to jest REMIS!')
+
